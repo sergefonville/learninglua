7f2db08a944d97502b16a85f8e49270fc00f1401
Serge Fonville <serge.fonville@gmail.com>
2020-08-15 18:31:49 +0200

    Part 2: Print and Comments
    Succeeding in Scripting! When and How to use Print and Comments
    This video gives a basic introduction about what can be done with
    the print command, how string concatenation works, a little implicit
    type conversion and the two types of comments (line- and block-)

---
+++ P2-Print.lua
@@ -0,0 +1,12 @@
+print("Hello World")
+print("Line 2", "Part 2")
+print(12)
+
+print('Hello') -- inline comment
+print("Hello" .. " " .. "World")
+
+-- This Comment
+--[[ this is a block comment
+it can span lines
+]]
+print("this will print")
