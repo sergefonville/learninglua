f4ecacc85d8c37835ba84e751fc587ac9846bb0f
Serge Fonville <serge.fonville@gmail.com>
2020-08-15 20:04:30 +0200

    Part 3: Variables
    Creating Variables and Variable Types in Lua
    This video explains the basic types of variables in Lua and
    how they behave, and in what ways they differ from other languages.
    I left all the variable assignments and print statements in the file,
    since that makes it clearer what exactly has been done in this video.

---
+++ P3-Variables.lua
@@ -0,0 +1,33 @@
+myVariable = nil
+print (type (myVariable))
+
+myInteger = 10
+print (myInteger)
+print (type (myInteger))
+myFloat = 3.14159265
+print (myFloat)
+print (type (myFloat))
+
+myString = "Hello"
+print (myString)
+print (type(myString))
+
+myString2 = [[ Multi-
+line
+strings
+are fun]]
+print (myString2)
+
+myBoo = true
+myBoo2 = false
+myBoo3 = nil
+print (myBoo3)
+print (type(myBoo3))
+
+myArray = {"a string", 12, 42, "14"}
+print (myArray)
+
+print (myArray[1])
+print (myArray[4])
+print (myArray[2])
+
