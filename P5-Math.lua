f82b228f63cd93c6f5f955cb835125aaba7111dc
sergefonville <serge.fonville@gmail.com>
2020-08-17 00:23:02 +0200

    Part 5: Math
    How to do Math and use the Math Library in Lua to make your life easier
    This video shows how to use the common math functions in Lua. An odd
    quirk I noticed was that the math.random() functions, was not very
    random at all. The use of math.randomseed(os.time()) did affect the
    value, but did not make it random. Some Googling resulted in the
    advice to call math.random() a couple of times, that did affect the
    resulting value.

---
+++ P5-Math.lua
@@ -0,0 +1,17 @@
+answer = (2+5)*4
+print (answer)
+
+answer2 = 5%4
+print (answer2)
+
+myPi = math.pi
+print (myPi)
+
+math.randomseed(os.time())
+myNumber = math.random(1,10)
+print (myNumber)
+
+mySin = math.sin(1)
+print (mySin)
+
+
