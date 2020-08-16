af1b6bd514650b5c78514299c2c624ccabc6382b
sergefonville <serge.fonville@gmail.com>
2020-08-16 20:41:34 +0200

    Part 4: Strings
    How to use Strings in Lua
    This video covers the use of strings in Lua. It shows how to
    interact with string, covers a little bit of assignment and how
    multiline strings are handled.

---
+++ P4-Strings.lua
@@ -0,0 +1,26 @@
+myString = "Hello"
+print (#myString)
+
+myOtherString = " World"
+print (myString .. myOtherString)
+
+myNumber = 42
+myStringNumber = tostring(myNumber) -- "42"
+print (myStringNumber)
+
+myFirstString = "Hi Mom"
+mySecondString = "Hi Dad"
+myThirdString = [[ Multi-line
+string
+that
+contains
+newline ]]
+
+myA, myB, myC = "hi", "hello"
+
+print (myA)
+print (myB)
+print (myC)
+
+myNewLine = "This is a string \n\twith a \"newline\""
+print (myNewLine)
